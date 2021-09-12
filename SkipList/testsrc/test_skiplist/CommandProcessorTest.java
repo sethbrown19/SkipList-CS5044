46f33af493d0d76117c8cdfc7cbee633df74f395
Seth Brown <sethbrown@Seths-MacBook-Air.local>
2021-09-11 20:41:24 -0400

    completed all JUnit testing for command processor. I will have to update when i complete Database class.

--- SkipList/testsrc/test_skiplist/CommandProcessorTest.java
+++ SkipList/testsrc/test_skiplist/CommandProcessorTest.java
@@ -24,5 +24,40 @@ public class CommandProcessorTest extends TestCase {
 
 		assertFuzzyEquals("Remove method for string", systemOut().getHistory());
 	}
+	
+	public void testProcessorRegionSearch() {
+		String line = "regionsearch";
+		processor.processor(line);
+
+		assertFuzzyEquals("Region search method", systemOut().getHistory());
+	}
+	
+	public void testProcessorSearch() {
+		String line = "search";
+		processor.processor(line);
+
+		assertFuzzyEquals("Search method", systemOut().getHistory());
+	}
+	
+	public void testProcessorIntersections() {
+		String line = "intersections";
+		processor.processor(line);
+
+		assertFuzzyEquals("Intersections method", systemOut().getHistory());
+	}
+	
+	public void testProcessorDump() {
+		String line = "dump";
+		processor.processor(line);
+
+		assertFuzzyEquals("Dump method", systemOut().getHistory());
+	}
+	
+	public void testProcessorInvalid() {
+		String line = "Invalid";
+		processor.processor(line);
+
+		assertFuzzyEquals("Not a command. Please input a proper command.", systemOut().getHistory());
+	}
 
 }
